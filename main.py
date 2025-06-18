f09efde1cdb9c950c45b6920ca7be3737a2524cb
Boomie Maddox <boomie@Boomies-MacBook-Pro.local>
2025-06-18 17:55:56 +0300

    Быстрое начало в Пайтон

---
+++ main.py
@@ -0,0 +1,36 @@
+# Добро пожаловать на быстрый старт по Пайтону!
+# 
+
+
+# дефолт числа класс int
+a = 10
+# дробные класс float
+c = 10.5
+# стринги класс str
+d = "Hello, World!"
+# list (итератор)
+g = [1, 2, 3]
+h = ["Python", "is", "awesome"]
+
+# if/else условие
+if a > 5:
+    print("a больше 5")
+
+# for цикл
+for i in range(5):
+    print(i)
+
+# while цикл
+i = 0
+while i < 5:
+    print(i)
+    i += 1
+
+# функция
+def func():
+    print("Это функция!")
+
+# c параметрами
+def func(a,b):
+    print("Сумма:", a + b)
+
